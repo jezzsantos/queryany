e552daf72e331d7b620d72b58eeae0c369cf0a7e
Jezz Santos <jezz@end-game.com>
2023-08-25 22:20:04 +1200

    Split Library solution from Reference solution

---
+++ samples/ri/GlobalAssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly:
+    AssemblyDescription("A reference implementation of a DDD CQRS API.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("jezzsantos")]
+[assembly: AssemblyProduct("QueryAny.Reference")]
+[assembly: AssemblyCopyright("Copyright 2023, jezzsantos")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0")]
