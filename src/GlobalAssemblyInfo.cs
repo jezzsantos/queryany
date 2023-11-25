6fa6fda4582bbeb8abd5cfab78feeef55a69b546
Jezz Santos <jezz.santos@hotmail.com>
2023-11-26 09:32:18 +1300

    #1.1.1

--- src/GlobalAssemblyInfo.cs
+++ src/GlobalAssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
-[assembly: AssemblyInformationalVersion("1.1.0")]
+[assembly: AssemblyFileVersion("1.1.1.0")]
+[assembly: AssemblyInformationalVersion("1.1.1")]
 
 [assembly: InternalsVisibleTo("QueryAny.UnitTests")]
