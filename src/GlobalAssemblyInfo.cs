33c1e49a3de18d44cb0b0145dcee55c2f94a6e9f
Jezz Santos <jezz.santos@hotmail.com>
2024-11-03 11:25:20 +1300

    #1.2.0

--- src/GlobalAssemblyInfo.cs
+++ src/GlobalAssemblyInfo.cs
@@ -8,13 +8,13 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("jezzsantos")]
 [assembly: AssemblyProduct("QueryAny")]
-[assembly: AssemblyCopyright("Copyright 2023, jezzsantos")]
+[assembly: AssemblyCopyright("Copyright 2024, jezzsantos")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.1.1.0")]
-[assembly: AssemblyInformationalVersion("1.1.1")]
+[assembly: AssemblyFileVersion("1.2.0.0")]
+[assembly: AssemblyInformationalVersion("1.2.0")]
 
 [assembly: InternalsVisibleTo("QueryAny.UnitTests")]
